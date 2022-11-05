39ac71aff04ab51246e4c9248ae3f77b05dd8257
MKazimierczak <117294104+MKazimierczak@users.noreply.github.com>
2022-11-05 13:10:14 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,45 @@
-﻿#include <iostream>
-#include "data.h"
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int odp = 0;
-
+void centerify_output(string str, int num_cols) {
+    // oblicz lewe dopełnienie
+    int left_padding = (num_cols / 2) - (str.size() / 2);
+    // wstawianie dopełnienia spacjami
+    for(int i = 0; i < left_padding; i++) cout << ' ';
+    // wypisanie wiadomości
+    cout << str;
+}
 int main() {
-    string imie = podaj_imie();
-    int miejsce = podaj_miejsce();
-    
+    vector<string> lines = {
+            "-----------------------------------------------------",
+            "|                     Welcome                       |",
+            "|                     to IOPS                       |",
+            "|                                                   |",
+            "|                                                   |",
+            "|                                                   |",
+            "|                 Powered by Ecorp                  |",
+            "-----------------------------------------------------",
+    };
+
+    int num_cols = 100;
+
+    // wirtualna granica
+    cout << endl;
+    for(int i = 0; i < num_cols; ++i) cout << ' ';
+    cout << '|' << endl;
+
+    // wypisanie
+    for(int i = 0; i < lines.size(); ++i){
+        centerify_output(lines[i], num_cols);
+        cout << endl;
+    }
+
+    // wirtualna granica
+    cout << endl;
+    for(int i = 0; i < num_cols; ++i) cout << ' ';
+    cout << '|' << endl;
+
     return 0;
 }
