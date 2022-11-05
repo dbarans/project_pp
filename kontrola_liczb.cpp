688cdddcb9e182e2fdecdba1bf52401548ea13a3
Dominik Baranski <baranskidominik109@gmail.com>
2022-11-05 19:15:13 +0100

    dodanie funkcji sprawdzajacej czy cin jest liczba

---
+++ kontrola_liczb.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "kontrola_liczb.h"
+using namespace std;
+
+int kontrola() {
+	int liczba;
+	bool blad = false;
+	do 
+	{
+		cin >> liczba;
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+			blad = true;
+			cout << "Podaj liczbe: ";
+		}
+		else
+		{
+			return liczba;
+		}
+	} while (blad);
+}
